9a927825b5e90d6cbb32ca39fd62f6eb062e14b2
LOOKCC <1397665447@qq.com>
2018-12-07 15:50:19 +0800

    20170903

--- 20170903.cpp
+++ 20170903.cpp
@@ -2,26 +2,80 @@
 #include<string>
 #include<vector>
 using namespace std;
-string process(vector<string> info, string relu){
-    
-    return 0;
+int find_next_end(string info, int now){
+    int count = 0;
+    for(int i=now; i<info.length(); i++){
+        if(info[i] == '{')
+            count++;
+        if(info[i] == '}')
+            count--;
+        if(count == 0)
+            return i;
+    }
+    return -1;
+}
+string delete_neg(string x){
+
+}
+string process(string info, string relu){
+    if(relu.find('.') != string::npos){
+        string pre = relu.substr(0, relu.find_first_of('.'));
+        if(info.find(pre) == string::npos){
+            return "NOTEXIT";
+        }
+        string after = relu.substr(relu.find_first_of('.')+1, relu.length()-relu.find_first_of('.')-1);
+        int pos = info.find(pre)+pre.length()+3;
+        int end  = find_next_end(info, pos);
+        if(end == -1)
+            return "error";//ee{er}ee
+        string this_object = info.substr(pos, end-pos+1);
+        // cout<<this_object<<endl;
+        return process(this_object, after);
+    }else{
+        if(info.find(relu) == string::npos){
+            return "NOTEXIT";
+        }
+        int pos = info.find(relu);
+        // cout<<info[pos+relu.length()+3]<<endl;
+        if(info[pos+relu.length()+3] == '{'){
+            return "OBJECT";
+        }
+        else{//"dd":"dd",
+             //0123456789
+             //pos = 1 length = 2 end = 9
+            int end = info.substr(pos, info.length()-pos).find_first_of(',');
+            if(end == string::npos){
+                end = info.substr(pos, info.length()-pos).find_first_of('}');
+            }
+            return info.substr(pos+relu.length()+4, end-5-relu.length());
+        }
+    }
 }
 int main(){
     int m,n;
     cin>>m>>n;
     cin.get();
-    vector<string> info;
+    string info;
     vector<string> relu;
     for(int i=0; i<m; i++){
         string temp;
         getline(cin, temp);
-        info.push_back(temp);
+        info += temp;
     }
     for(int i=0; i<n; i++){
         string temp;
         getline(cin, temp);
         relu.push_back(temp);
     }
-    
+    for(int i=0; i<relu.size(); i++){
+        string res = process(info, relu[i]);
+        if(res == "NOTEXIT")
+            cout<<res<<endl;
+        else if(res == "OBJECT")
+            cout<<res<<endl;
+        else{
+            cout<<"STRING "<<res<<endl;
+        }
+    }
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string temp="\"hello\"";
+    cout<<temp<<endl;
+    return 0;
+}
