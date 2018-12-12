6e4a450f063f87022c7797aadaf9ef59011bb4d7
LOOKCC <1397665447@qq.com>
2018-12-12 08:58:49 +0800

    to_win

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wzh/code/ccf
-# Build directory: /home/wzh/code/ccf/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
